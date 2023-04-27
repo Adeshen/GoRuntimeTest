c5732efc6679bb92d1e9787baec6e83ff611b171
herryzhong <878600556@qq.com>
2023-04-27 15:37:43 +0800

    go proc test modified

--- process_communicate/server.go
+++ process_communicate/server.go
@@ -9,15 +9,18 @@ type H struct {
 }
 
 func (H) S() {
-	fmt.Println("hello")
+	fmt.Println("hello  ")
 }
 
 func main() {
 	fmt.Println("server_start")
 	runtime.Tcase = H{}
 
-	go func() {
+	he := make(chan int)
 
-	}()
+	go func(i int) {
+		fmt.Println(i)
+	}(10)
 	// fmt.Print(runtime.runtimeInitTime)
+	<-he
 }
