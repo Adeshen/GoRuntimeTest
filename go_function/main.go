9720282ba171182ff69a15b53d7b3a36a0f2359a
herryzhong <878600556@qq.com>
2023-05-18 14:36:03 +0800

    base change

--- go_function/main.go
+++ go_function/main.go
@@ -7,10 +7,25 @@ import (
 	"os"
 )
 
+type Args struct {
+	A int
+	B int
+}
+
+type MyService struct {
+}
+
+func (*MyService) Multiply(A int, B int) int {
+	return A * B
+}
+
 func child_node(register_ip string, register_port string, child_port string) {
 	myService := new(MyService)
 	// 注册RPC服务
+
+	//  hash <uuid, reflect.type>
 	rpc.Register(myService)
+	// 找到函数
 
 	// 启动RPC服务监听
 	listener, err := net.Listen("tcp", ":"+child_port)
@@ -22,6 +37,7 @@ func child_node(register_ip string, register_port string, child_port string) {
 
 	register_service := func(register_ip string, register_port string) {
 		// 待填写
+
 	}
 	go register_service(register_ip, register_port)
 
@@ -32,6 +48,10 @@ func child_node(register_ip string, register_port string, child_port string) {
 			continue
 		}
 		go rpc.ServeConn(conn)
+
+		// 根据哈希码找到对应函数并运行
+		// 待处理
+
 	}
 }
 
@@ -59,6 +79,8 @@ func send_request(register_ip string, register_port string) {
 	fmt.Println("结果:", reply)
 }
 
+var node_level bool
+
 func main() {
 	// 获取命令行参数
 	fmt.Println("命令行参数数量:", len(os.Args))
@@ -67,11 +89,21 @@ func main() {
 	}
 	if len(os.Args) >= 5 && os.Args[1] == "child" {
 		//子节点
+		node_level = false
 		child_node(os.Args[2], os.Args[3], os.Args[4])
 		return
 	} else {
 		//注册节点
+		node_level = true
 		go main_node(os.Args[2], os.Args[3])
 	}
+
+	go func() {
+		if node_level {
+			send_request(os.Args[2], os.Args[3])
+			return
+		}
+
+	}()
 
 }
